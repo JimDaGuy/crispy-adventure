c05c5e75a5076d64ecaa927b0f720e22fd8177de
James Digrazia <jtd2401@rit.edu>
2019-01-29 16:52:20 -0500

    Implement Redis store with express-session

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,61 @@
 const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const csrf = require('csurf');
 const os = require('os');
 
 const port = process.env.PORT || process.env.NODE_PORT || 8080;
+
+let redisURL = {
+  hostname: 'localhost',
+  port: 6379
+};
+
+let redisPass;
+
+if (process.env.REDISCLOUD_URL) {
+  redisURL = url.parse(process.env.REDISCLOUD_URL);
+  redisPass = redisURL.auth.split(':')[1]; // eslint-disable-line prefer-destructuring
+}
 
 const router = require('./router.js');
 
 const app = express();
 
 app.use(express.static('dist'));
+
+app.disable('x-powered-by');
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  session({
+    key: 'sessionid',
+    store: new RedisStore({
+      host: redisURL.hostname,
+      port: redisURL.port,
+      pass: redisPass
+    }),
+    secret: 'Paint Gauge Super Duper Secret',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true
+    }
+  })
+);
+app.use(cookieParser());
+
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCSRFTOKEN') return next(err);
+
+  console.log('Missing CSRF token');
+  return false;
+});
 
 router(app);
 
